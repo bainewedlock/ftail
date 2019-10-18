80af4290cdf854292bb998bc127229d0affef946
boeckwi <w.boeck@walz.de>
2019-10-18 09:35:31 +0200

    chocolatey package

---
+++ Chocolatey_Package/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'FTailSetup.msi'
+$url        = ''
+$url64      = ''
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+  softwareName  = 'FTail*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs
