7d5598051ba0d454d3f1b08541d223c796ba6220
bainewedlock <bainewedlock@gmail.com>
2019-10-24 12:06:21 +0200

    choco troubleshooting

--- Chocolatey_Package/tools/chocolateyinstall.ps1
+++ Chocolatey_Package/tools/chocolateyinstall.ps1
@@ -1,21 +1,15 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$packageName = 'ftail'
+$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'FTailSetup.msi'
-$url        = ''
-$url64      = ''
+
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url           = $url
-  url64bit      = $url64
-  file         = $fileLocation
-  softwareName  = 'FTail*'
-  checksum      = ''
-  checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  packageName   = $packageName
+  fileType      = 'msi'
+  file          = $fileLocation
+  silentArgs    = "/qn /norestart /l*v c:\FTail_msi_install.log"
   validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'FTail'
 }
+
 Install-ChocolateyInstallPackage @packageArgs
