c0b30710842e8b3f9a9812db754af4617274b50e
bainewedlock <bainewedlock@gmail.com>
2019-10-24 16:46:57 +0200

    choco troubleshooting

--- Chocolatey_Package/tools/chocolateyinstall.ps1
+++ Chocolatey_Package/tools/chocolateyinstall.ps1
@@ -7,8 +7,9 @@ $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
   file          = $fileLocation
-  silentArgs    = "/qn /norestart /l*v c:\FTail_msi_install.log"
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
+
   softwareName  = 'FTail'
 }
 
